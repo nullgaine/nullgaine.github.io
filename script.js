2fe12d449857adf5293de853311b1209a83c7940
BBTT16 <haju0106@gmail.com>
2025-09-13 14:04:25 +0900

    Update script.js
    버튼 스크립트 오류 수정

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function backToTop() {
-  const position =
-    document.documentElement.scrollTop || document.body.scrollTop;
-  if (position) {
+ function backToTop() {
+   const position =
+     document.documentElement.scrollTop || document.body.scrollTop;
+   if (position) {
     window.requestAnimationFrame(() => {
-      window.scrollTo(0, position - position / 10);
-      backToTop();
-    });
-  }
+       window.scrollTo( {
+        top: 0,
+        behavior: 'smooth'
+       } );
+     });
+   }
 }  //페이지 위로 올리기 + 애니메이션 추가 오래된 T스토리에서 발굴(감사합니다)
+
